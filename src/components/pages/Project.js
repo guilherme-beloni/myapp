a3cb844090eff14c6ec546f14b65603d0264b364
Guilherme Zacarias Beloni <97005507+guilherme-beloni@users.noreply.github.com>
2024-03-11 22:27:54 -0300

    att_patch_project

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,10 +1,17 @@
 import styles from "./Project.module.css";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import Container from "../layout/Container";
+import Message from "../layout/Message";
+import ProjectForm from "../project/ProjectForm";
 
 function Project() {
   const { id } = useParams();
+
   const [project, setProject] = useState({}); // Declare a variável project
+  const [showProjectForm, setShowProjectForm] = useState(false); // Declare a variável project
+  const [message, setMessage] = useState();
+  const [type, setType] = useState(); // Declare a variável
 
   useEffect(() => {
     fetch(`http://localhost:5000/projects/${id}`, {
@@ -20,7 +27,68 @@ function Project() {
       .catch((err) => console.error(err));
   }, [id]);
 
-  return <p>{project.name}</p>;
+  function editPost(project) {
+    // budget validation
+    fetch(`http://localhost:5000/projects/${project.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "aplication/json",
+      },
+      body: JSON.stringify(project),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setProject(data);
+        setShowProjectForm(false);
+        setMessage("Projeto Atualizado!");
+        setType("success");
+      })
+      .catch((err) => console.log(err));
+  }
+
+  function toggleProjectForm() {
+    setShowProjectForm(!showProjectForm);
+  }
+
+  return (
+    <>
+      {project.name ? (
+        <div className={styles.project_details}>
+          <Container customClass="column">
+            {message && <Message type={type} msg={message} />}
+            <div className={styles.details_container}>
+              <h1>Projeto: {project.name}</h1>
+              <button className={styles.btn} onClick={toggleProjectForm}>
+                {!showProjectForm ? "Editar projeto" : "Fechar"}
+              </button>
+              {!showProjectForm ? (
+                <div className={styles.project_info}>
+                  <p>
+                    <span>Categoria: </span>
+                    {project.category.name}
+                  </p>
+                  <p>
+                    <span>Custo: </span>
+                    R${project.budget}
+                  </p>
+                </div>
+              ) : (
+                <div className={styles.project_info}>
+                  <ProjectForm
+                    handleSubmit={editPost}
+                    btnText="Concluir"
+                    projectData={project}
+                  />
+                </div>
+              )}
+            </div>
+          </Container>
+        </div>
+      ) : (
+        "Carregando..."
+      )}
+    </>
+  );
 }
 
 export default Project;
